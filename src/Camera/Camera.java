e18d82c71a9a4f54608ca3013e4b0842077eab99
ducpro100520 <62461886+ducpro100520@users.noreply.github.com>
2021-05-09 10:35:10 +0700

    update camera

--- src/Camera/Camera.java
+++ src/Camera/Camera.java
@@ -20,27 +20,6 @@ public class Camera extends Points{
     Cal c = new Cal();
     //check a point if it's in the camerazone
     public boolean isCameraZone(Points aPoint, ArrayList<Points> p){
-       Plane x1 = new Plane(p.get(0),p.get(1),p.get(2));
-       Plane x2 = new Plane(p.get(0),p.get(2),p.get(3));
-       Plane x3 = new Plane(p.get(0),p.get(3),p.get(4));
-       Plane x4 = new Plane(p.get(0),p.get(4),p.get(1));
-       Plane x5 = new Plane(p.get(1),p.get(2),p.get(3));
-       if(c.IsInPlane(aPoint, x1)){
-           return true;
-       }
-       else if(c.IsInPlane(aPoint, x2)){
-           return true;
-       }
-       else if(c.IsInPlane(aPoint, x3)){
-           return true;
-       }
-       else if(c.IsInPlane(aPoint, x4)){
-            return true;
-        }
-        else if(c.IsInPlane(aPoint, x5)){
-            return true;
-        }
-        else{
             Arraylist<Points> p1 = new ArrayList<Points>();
             Arraylist<Points> p2 = new ArrayList<Points>();
             Arraylist<Points> p3 = new ArrayList<Points>();
@@ -52,14 +31,14 @@ public class Camera extends Points{
            p4.add(p.get(0));    p4.add(p.get(4));   p4.add(p.get(1));
            p5.add(p.get(1));    p5.add(p.get(2));   p5.add(p.get(3));   p5.add(p.get(4));
            double camVolume = c.GetVolPyr(p5, p.get(0));
-           double sumVolume = c.get(p1, aPoint) + c.get(p2, aPoint) + c.get(p3, aPoint) + c.get(p4, aPoint) + c.get(p5, aPoint);
+           double sumVolume = c.GetVolPyr(p1, aPoint) + c.GetVolPyr(p2, aPoint) + c.GetVolPyr(p3, aPoint) + c.GetVolPyr(p4, aPoint) + c.GetVolPyr(p5, aPoint);
+           //lam tron gia tri
            double a = (double)Math.round(camVolume * 100)/100;
            double b = (double)Math.round(sumVolume * 100)/100;
            if(a == b){
                return true;
            }
            return false;
-        }
     }
 
 }
